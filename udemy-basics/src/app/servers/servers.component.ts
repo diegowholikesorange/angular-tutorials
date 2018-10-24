17a5f10138e282a1b1d8c85494324abcdf93e5ad
Diego Tognola <22850806+diegowholikesorange@users.noreply.github.com>
2018-10-24 21:24:55 +1100

    databinding

--- udemy-basics/src/app/servers/servers.component.ts
+++ udemy-basics/src/app/servers/servers.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<p>this is inline now</p>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent implements OnInit {
+export class ServersComponent {
 
-  constructor() { }
+  message = "Server is up"
+  addDisabled = true
+  serverName
 
-  ngOnInit() {
+  constructor() {
+    setTimeout(() => this.addDisabled = false, 2000)
   }
 
+  getTime() {
+    return Date.now();
+  }
+
+  onAddButtonClick() {
+    this.addDisabled = true
+    console.log("Add button was clicked")
+    console.log()
+  }
+
+  onMouseOver() {
+    console.log("Button under mouse")
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value
+    console.log(this.serverName)
+  }
 }
